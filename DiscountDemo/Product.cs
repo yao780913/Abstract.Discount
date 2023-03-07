c240e5bb04de0675bba4b5fe20becb671fd501df
bill.wang <bill.wang@edgentauems.com>
2023-03-07 17:37:33 +0800

    [feat] define product class and output text

---
+++ DiscountDemo/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscountDemo;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string SKU { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public HashSet<string> Tags { get; set; }
+}
---
+++ DiscountDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using System.Text.Json;
+using DiscountDemo;
+
+var products = LoadProducts();
+
+foreach (var p in products)
+{
+    Console.WriteLine($"- {p.Name}      {p.Price:C}");
+}
+
+Console.WriteLine($"Total: {CheckoutProcess(products.ToArray()):C}");
+
+Console.ReadLine();
+
+decimal CheckoutProcess (Product[] products)
+{
+    return products.Sum(p => p.Price);
+}
+
+IEnumerable<Product> LoadProducts ()
+{
+    var text = File.ReadAllText(@"products.json", Encoding.UTF8);
+    return JsonSerializer.Deserialize<Product[]>(text, new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    });
+}
