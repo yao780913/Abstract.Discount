13e23d77c34155e9d24f62fa6e4f935b61cf784a
bill.wang <bill.wang@edgentauems.com>
2023-03-08 14:36:08 +0800

    [feat] add ExclusiveTag in RuleBase.cs

--- DiscountDemo/DiscountRules/RuleBase.cs
+++ DiscountDemo/DiscountRules/RuleBase.cs
@@ -3,8 +3,17 @@
 public abstract class RuleBase
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Note { get; set; }
+    public string Name { get; protected init; }
+    public string Note { get; protected init; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// 如果值不等於 null, 表示此折扣為 獨佔折扣, 
+    /// 若多個折扣規則有相同的 ExclusiveTag, 則這些折扣只能套用一個。 
+    /// </remarks>
+    public string? ExclusiveTag = null;
 
     public abstract IEnumerable<Discount> Process (CartContext cart);
 }
